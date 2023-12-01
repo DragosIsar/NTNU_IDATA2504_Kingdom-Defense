26ebcdf06243d249d87c638eaf4038aefb47ae58
Me9Rs <malte.rudius@web.de>
2023-12-01 14:30:57 +0100

    feat: Day/Night cycle on directional light
    - Put the day/night cycle script directly on the directional light
    - Adjusted light temperature depending on rotation
    - Adjusted light intensity depending on rotation
    - Saved Directional Light as Prefab

--- Assets/_Project/Scripts/Level/DayNightCycle.cs
+++ Assets/_Project/Scripts/Level/DayNightCycle.cs
@@ -4,17 +4,16 @@ namespace _Project.Scripts
 {
     public class DayNightCycle : MonoBehaviour
     {
-        private GameObject directionalLight;
-
         private Vector3 _rot = Vector3.zero;
         public float rotationSpeedInDeg = 6;
 
         [SerializeField] private bool isDay = true;
+        private Light _light;
         public bool IsDay => isDay;
 
         private void Start()
         {
-            directionalLight = GameObject.Find("Directional Light");
+            _light = GetComponent<Light>();
         }
 
         private void Update()
@@ -25,9 +24,16 @@ namespace _Project.Scripts
         private void Cycle()
         {
             _rot.x = rotationSpeedInDeg * Time.deltaTime;
-            directionalLight.transform.Rotate(_rot);
+            Transform t;
+            (t = transform).Rotate(_rot);
 
-            isDay = !(directionalLight.transform.rotation.eulerAngles.x > 90);
+            Quaternion rotation = t.rotation;
+            isDay = !(rotation.eulerAngles.x > 90);
+
+            _light.colorTemperature = Mathf.Lerp(1000, 10000, rotation.eulerAngles.x / 135);
+
+            float lightIntensity = !isDay ? 0 : Mathf.Lerp(0, 1, Mathf.Clamp01(rotation.eulerAngles.x / 90));
+            _light.intensity = lightIntensity;
         }
     }
 }
